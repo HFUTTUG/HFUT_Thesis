440fc0235f090f60c110fc6ef5368ae3a9ee79d1
Teng Li <tengli.terry+dev@gmail.com>
2022-04-10 00:30:48 +0800

    refactor: Remove test modules
    * test modules will be rewrite in the future
    
    Signed-off-by: Teng Li <tengli.terry+dev@gmail.com>

--- test/config-nomencl.lua
+++
@@ -1,8 +0,0 @@
-testfiledir = "./test/testfiles-nomencl"
-testsuppdir = testfiledir .. "/support"
-
-checkruns = 2
-
-function runtest_tasks(name)
-    return "makeindex -s nomencl.ist -o " .. name .. ".nls " .. name .. ".nlo"
-end
